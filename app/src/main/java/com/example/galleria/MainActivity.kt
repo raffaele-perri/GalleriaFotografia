08b4a115edbba1cbd94a6cdbf9cf427790e7250b
raffaeleperri <raffaele.perri.rp@gmail.com>
2021-06-22 18:02:45 +0200

    toolbar

--- app/src/main/java/com/example/galleria/MainActivity.kt
+++ app/src/main/java/com/example/galleria/MainActivity.kt
@@ -1,19 +1,14 @@
 package com.example.galleria
 
-import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.util.AttributeSet
-import android.util.Log
-import android.view.View
-import androidx.navigation.NavController
-import androidx.navigation.Navigation
-import androidx.navigation.findNavController
+import androidx.appcompat.widget.Toolbar
 import androidx.navigation.fragment.NavHostFragment
-import androidx.navigation.fragment.findNavController
+
+import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.setupWithNavController
 import com.google.android.material.bottomnavigation.BottomNavigationView
-import com.google.android.material.navigation.NavigationView
+
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -22,13 +17,14 @@ class MainActivity : AppCompatActivity() {
         val host: NavHostFragment = supportFragmentManager.findFragmentById(R.id.fragmentContainerView)
                 as NavHostFragment
         val navController = host.navController
-        setupBottomNavMenu(navController)
 
-    }
-
-    private fun setupBottomNavMenu(navController: NavController) {
         val bottomNav = findViewById<BottomNavigationView>(R.id.bottomNavigationView)
-        bottomNav?.setupWithNavController(navController)
+        bottomNav.setupWithNavController(navController)
+
+        val appBarConfiguration = AppBarConfiguration(navController.graph)
+        val toolbar = findViewById<Toolbar>(R.id.toolbar)
+        toolbar.setupWithNavController(navController,appBarConfiguration)
+
     }
 
 }
