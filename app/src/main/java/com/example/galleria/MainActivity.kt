3aea1ca47327032eb9d477c373b93b1e5341c5dc
raffaeleperri <raffaele.perri.rp@gmail.com>
2021-06-22 14:52:04 +0200

    navigazione di base

--- app/src/main/java/com/example/galleria/MainActivity.kt
+++ app/src/main/java/com/example/galleria/MainActivity.kt
@@ -1,11 +1,34 @@
 package com.example.galleria
 
+import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.AttributeSet
+import android.util.Log
+import android.view.View
+import androidx.navigation.NavController
+import androidx.navigation.Navigation
+import androidx.navigation.findNavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.fragment.findNavController
+import androidx.navigation.ui.setupWithNavController
+import com.google.android.material.bottomnavigation.BottomNavigationView
+import com.google.android.material.navigation.NavigationView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val host: NavHostFragment = supportFragmentManager.findFragmentById(R.id.fragmentContainerView)
+                as NavHostFragment ?: return
+        val navController = host.navController
+        setupBottomNavMenu(navController)
+
     }
+
+    private fun setupBottomNavMenu(navController: NavController) {
+        val bottomNav = findViewById<BottomNavigationView>(R.id.bottomNavigationView)
+        bottomNav?.setupWithNavController(navController)
+    }
+
 }
