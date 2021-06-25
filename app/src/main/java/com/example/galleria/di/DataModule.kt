995abb0e80c7675beb3223b7359e72e5476cbc1a
raffaeleperri <raffaele.perri.rp@gmail.com>
2021-06-25 16:17:51 +0200

    datamodule

---
+++ app/src/main/java/com/example/galleria/di/DataModule.kt
@@ -0,0 +1,21 @@
+package com.example.galleria.di
+
+import android.content.Context
+import androidx.room.Room
+import com.example.galleria.framework.database.AppDatabase
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.android.qualifiers.ApplicationContext
+import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+internal class DataModule {
+
+    @Provides
+    @Singleton
+    fun provideDatabase(@ApplicationContext context: Context ) : AppDatabase =
+        Room.databaseBuilder(context,AppDatabase::class.java, "database_Birre").build()
+}
